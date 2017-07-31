70b4d8bd84e661e812853aeed3066ec96ffb88c5
Diego999 <diegoantognini@gmail.com>
2017-07-31 11:18:16 +0200

    Refactoring extracted dates

--- process_data.py
+++ process_data.py
@@ -25,22 +25,15 @@ def process_folder(folder):
 
         # Extract & clean release date
         extracted_release_date = parser_utils.extract_release_date(buffer)
-        if extracted_release_date is not None:
-            info[config.KEY_RELEASED_DATE] = parser_utils.clean_date(extracted_release_date, year_annual_report)
-        else:
-            with open(config.LOG_RELEASE_DATE_MISSING, 'a', encoding='utf-8') as fp:
-                fp.write(annual_report + '\n')
+        info[config.KEY_RELEASED_DATE] = parser_utils.clean_date(extracted_release_date, year_annual_report)
 
         # Extract & clean fiscal year end
         extracted_fiscal_year_end = parser_utils.extract_fiscal_end_year(buffer)
         if extracted_fiscal_year_end is not None:
             info[config.KEY_FISCAL_YEAR_END] = parser_utils.clean_date(extracted_fiscal_year_end, year_annual_report)
-        else:
-            prefix = ''
-            # Infer the date
-            if extracted_release_date is not None:
-                prefix = 'FIXED '
-                info[config.KEY_FISCAL_YEAR_END] = parser_utils.clean_date('31 12 ' + str(year_annual_report))
+        elif extracted_release_date is not None: # Infer the date
+            info[config.KEY_FISCAL_YEAR_END] = parser_utils.clean_date('31 12 ' + str(year_annual_report))
+
         # Extract beginning of sections
         val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9 = parser_utils.extract_items(buffer)
 
@@ -84,9 +77,8 @@ def process_folder(folder):
             val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9 = parser_utils.remove_first_ref_previous_item([val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9])
             val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9 = parser_utils.remove_last_ref_next_item([val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9])
 
+        print(extracted_release_date, extracted_fiscal_year_end, val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9)
 
-            with open(config.LOG_FISCAL_YEAR_END_MISSING, 'a', encoding='utf-8') as fp:
-                fp.write(prefix + annual_report + '\n')
 
 if __name__ == "__main__":
     random.seed(0)
