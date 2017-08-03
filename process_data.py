87fd14d9ecfb21b56ac54fa35605123b94c70d39
Diego999 <diegoantognini@gmail.com>
2017-08-03 10:44:17 +0200

    Refactor

--- process_data.py
+++ process_data.py
@@ -48,6 +48,48 @@ def update_key_item(info, val, key1, key2, key3):
             info[key3] = ', '.join([str(v) for v in val[2:]])
 
 
+def extract(buffer, cik, annual_report, names):
+    info = {config.KEY_FILE: cik + '/' + annual_report[annual_report.rfind('/') + 1:], config.KEY_COMPANY_NAME: ', '.join(names), config.KEY_CIK: cik, config.KEY_PARSING: False}
+    if len(buffer) > 0:
+        year_annual_report = annual_report.split('-')[-2]
+
+        # Extract & clean release date
+        extracted_release_date = parser_utils.extract_release_date(buffer)
+        if extracted_release_date is not None:
+            extracted_release_date = parser_utils.clean_date(extracted_release_date, year_annual_report)
+            if extracted_release_date is not None:
+                info[config.KEY_RELEASED_DATE] = extracted_release_date
+
+        # Extract & clean fiscal year end
+        extracted_fiscal_year_end = parser_utils.extract_fiscal_end_year(buffer)
+        if extracted_fiscal_year_end is not None:
+            extracted_fiscal_year_end = parser_utils.clean_date(extracted_fiscal_year_end, year_annual_report)
+            if extracted_fiscal_year_end is not None:
+                info[config.KEY_FISCAL_YEAR_END] = extracted_fiscal_year_end
+        elif extracted_release_date is not None:  # Infer the date
+            extracted_fiscal_year_end = parser_utils.clean_date('31 12 ' + str(year_annual_report), year_annual_report)
+            if extracted_fiscal_year_end is not None:
+                info[config.KEY_FISCAL_YEAR_END] = extracted_fiscal_year_end
+
+        # Extract beginning of sections
+        vals = parser_utils.extract_items(buffer)
+        if vals is not None:
+            val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9 = vals
+            update_key_item(info, val_1a, config.KEY_ITEM_1A_1, config.KEY_ITEM_1A_2, config.KEY_ITEM_1A_3)
+            update_key_item(info, val_1b, config.KEY_ITEM_1B_1, config.KEY_ITEM_1B_2, config.KEY_ITEM_1B_3)
+            update_key_item(info, val_2, config.KEY_ITEM_2_1, config.KEY_ITEM_2_2, config.KEY_ITEM_2_3)
+            update_key_item(info, val_7, config.KEY_ITEM_7_1, config.KEY_ITEM_7_2, config.KEY_ITEM_7_3)
+            update_key_item(info, val_7a, config.KEY_ITEM_7A_1, config.KEY_ITEM_7A_2, config.KEY_ITEM_7A_3)
+            update_key_item(info, val_8, config.KEY_ITEM_8_1, config.KEY_ITEM_8_2, config.KEY_ITEM_8_3)
+            update_key_item(info, val_9, config.KEY_ITEM_9_1, config.KEY_ITEM_9_2, config.KEY_ITEM_9_3)
+        else:
+            info[config.KEY_PARSING] = True
+    else:
+        info[config.KEY_PARSING] = True
+
+    return info
+
+
 def process_folder(folder, connection):
     cik = str(folder[folder.rfind('/') + 1:])
     names = []
@@ -61,43 +103,8 @@ def process_folder(folder, connection):
             buffer = load_annual_report(annual_report)
         except:
             continue
-        info = {config.KEY_FILE: cik + '/' + annual_report[annual_report.rfind('/') + 1:], config.KEY_COMPANY_NAME: ', '.join(names), config.KEY_CIK: cik, config.KEY_PARSING: False}
-        if len(buffer) > 0:
-            year_annual_report = annual_report.split('-')[-2]
 
-            # Extract & clean release date
-            extracted_release_date = parser_utils.extract_release_date(buffer)
-            if extracted_release_date is not None:
-                extracted_release_date = parser_utils.clean_date(extracted_release_date, year_annual_report)
-                if extracted_release_date is not None:
-                    info[config.KEY_RELEASED_DATE] = extracted_release_date
-
-            # Extract & clean fiscal year end
-            extracted_fiscal_year_end = parser_utils.extract_fiscal_end_year(buffer)
-            if extracted_fiscal_year_end is not None:
-                extracted_fiscal_year_end = parser_utils.clean_date(extracted_fiscal_year_end, year_annual_report)
-                if extracted_fiscal_year_end is not None:
-                    info[config.KEY_FISCAL_YEAR_END] = extracted_fiscal_year_end
-            elif extracted_release_date is not None: # Infer the date
-                extracted_fiscal_year_end = parser_utils.clean_date('31 12 ' + str(year_annual_report), year_annual_report)
-                if extracted_fiscal_year_end is not None:
-                    info[config.KEY_FISCAL_YEAR_END] = extracted_fiscal_year_end
-
-            # Extract beginning of sections
-            vals = parser_utils.extract_items(buffer)
-            if vals is not None:
-                val_1a, val_1b, val_2, val_7, val_7a, val_8, val_9 = vals
-                update_key_item(info, val_1a, config.KEY_ITEM_1A_1, config.KEY_ITEM_1A_2, config.KEY_ITEM_1A_3)
-                update_key_item(info, val_1b, config.KEY_ITEM_1B_1, config.KEY_ITEM_1B_2, config.KEY_ITEM_1B_3)
-                update_key_item(info, val_2, config.KEY_ITEM_2_1, config.KEY_ITEM_2_2, config.KEY_ITEM_2_3)
-                update_key_item(info, val_7, config.KEY_ITEM_7_1, config.KEY_ITEM_7_2, config.KEY_ITEM_7_3)
-                update_key_item(info, val_7a, config.KEY_ITEM_7A_1, config.KEY_ITEM_7A_2, config.KEY_ITEM_7A_3)
-                update_key_item(info, val_8, config.KEY_ITEM_8_1, config.KEY_ITEM_8_2, config.KEY_ITEM_8_3)
-                update_key_item(info, val_9, config.KEY_ITEM_9_1, config.KEY_ITEM_9_2, config.KEY_ITEM_9_3)
-            else:
-                info[config.KEY_PARSING] = True
-        else:
-            info[config.KEY_PARSING] = True
+        info = extract(buffer, cik, annual_report, names)
 
         with connection.cursor() as cursor:
             sql = "INSERT INTO `10k` ({}) VALUES ({})"
