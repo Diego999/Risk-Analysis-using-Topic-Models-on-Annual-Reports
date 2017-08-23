af3617b6945f1916a32daee1db5b9b32827f99ef
Diego999 <diegoantognini@gmail.com>
2017-08-23 10:04:41 +0200

    Remove useless config.SEED assignment

--- 4a_analyze_topics_static.py
+++ 4a_analyze_topics_static.py
@@ -449,8 +449,6 @@ if __name__ == "__main__":
                 tau_range = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]  # Offset
 
                 for i in range(max_try):
-                    config.SEED = random.randint(1, 100000)
-
                     alpha = utils.draw_val(alpha_range)
                     eta = utils.draw_val(eta_range)
                     num_topics = utils.draw_val(topic_range)
