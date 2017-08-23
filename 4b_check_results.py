150ad07c0b4a9d1831112e3824031adeda600f42
Diego999 <diegoantognini@gmail.com>
2017-08-23 13:49:27 +0200

    Improve reports for 4b_check_results

--- 4b_check_results.py
+++ 4b_check_results.py
@@ -6,6 +6,8 @@ config = __import__('0_config')
 
 
 def check_param_topic(k=8):
+    print('Plot Analysis')
+    print('')
     results = glob.glob(os.path.join(config.OUTPUT_FOLDER, 'topics') + '/*.txt')
     parsed_data = {}
     for r in results:
@@ -33,7 +35,7 @@ def check_param_topic(k=8):
     num_topics = {section:[int(t['topics']) for t in parsed_data_] for section, parsed_data_ in parsed_data.items()}
     x = range(min([min(v) for v in num_topics.values()]), max([max(v) for v in num_topics.values()]) + 1)
     for section, parsed_data_ in parsed_data.items():
-        print('{} best in {}'.format(k, section))
+        print('{} best in Section {}'.format(k, section))
         print('cv')
         for t in sorted(parsed_data_, key=lambda x:-x['cv'])[:k]:
             print('\t', t)
@@ -67,29 +69,44 @@ def check_param_topic(k=8):
         plt.ylabel('Coherence score (CU)')
         plt.xticks(np.arange(min(x) - 1, max(x) + 1, 5.0))
 
-        plt.show()
+        plt.draw()
+    plt.show()
+    print('')
+    print('')
+    print('')
 
 
 def check_tuning(k=3):
-    results = glob.glob(config.OUTPUT_FOLDER + '/tuning/*.txt')
-    parsed_data = []
+    print('Tuning analysis')
+    print('')
+    results = glob.glob(os.path.join(config.OUTPUT_FOLDER, 'old-tuning') + '/*.txt')
+    parsed_data = {}
     for r in results:
         vals = r[:r.rfind('.')].split('_')
+        section = vals[0]
+        section = section[section.rfind('/') + 1:]
         t = {k:v for k,v in [v.split(':') for v in vals if ':' in v]}
-        parsed_data.append(t)
 
-    parsed_data_sorted_by_cu = sorted(parsed_data, key=lambda x:-float(x['cu']))
-    parsed_data_sorted_by_cv = sorted(parsed_data, key=lambda x:-float(x['cv']))
+        if section not in parsed_data:
+            parsed_data[section] = []
+        parsed_data[section].append(t)
 
-    for i, (cu, cv) in enumerate(zip(parsed_data_sorted_by_cu[:k], parsed_data_sorted_by_cv[:k])):
-        cu_sorted = sorted(cu.items(), key=lambda x:x[0])
-        cv_sorted = sorted(cv.items(), key=lambda x:x[0])
+    parsed_data_sorted_by_cu = {section:sorted(parsed_data_, key=lambda x:-float(x['cu'])) for section, parsed_data_ in parsed_data.items()}
+    parsed_data_sorted_by_cv = {section:sorted(parsed_data_, key=lambda x:-float(x['cv'])) for section, parsed_data_ in parsed_data.items()}
 
-        print('Best #{} (cu, cv)'.format(i+1))
-        for a, b in zip(cu_sorted, cv_sorted):
-            assert a[0] == b[0]
-            print('\t', '\t', a[0], '\t', '\t', round(a[1], 4) if not isinstance(a[1], str) else a[1], '\t', '\t', round(b[1], 4) if not isinstance(b[1], str) else b[1])
+    for section in parsed_data.keys():
+        print('Section {}'.format(section))
+        for i, (cu, cv) in enumerate(zip(parsed_data_sorted_by_cu[section][:k], parsed_data_sorted_by_cv[section][:k])):
+            cu_sorted = sorted(cu.items(), key=lambda x:x[0])
+            cv_sorted = sorted(cv.items(), key=lambda x:x[0])
 
+            print('Best #{} (cu, cv)'.format(i+1))
+            for a, b in zip(cu_sorted, cv_sorted):
+                assert a[0] == b[0]
+                print('\t', '\t', a[0], '\t', '\t', round(a[1], 4) if not isinstance(a[1], str) else a[1], '\t', '\t', round(b[1], 4) if not isinstance(b[1], str) else b[1])
+    print('')
+    print('')
+    print('')
 
 if __name__ == "__main__":
     check_param_topic()
