4c298d5631e14727503905c0526ae327a70b60c7
Diego999 <diegoantognini@gmail.com>
2017-08-28 14:48:50 +0200

    Compute also topic distribution per document

--- 4d_infer_topics.py
+++ 4d_infer_topics.py
@@ -28,12 +28,14 @@ def get_topics(num_topics, model, idx_to_lemma, output):
             for l, p in lemmas_probs:
                 fp.write('\t{}\t{}\n'.format(l, p))
 
+    return topics
+
 
 def get_terms_topics(model, idx_to_lemma, output):
     # Get terms distribution w.r.t. to each topics
     terms_topics = []
     for lemma_id, lemma in idx_to_lemma.items():
-        terms_topics.append((lemma, model.get_term_topics(lemma_id, 1e-20)))
+        terms_topics.append((lemma, model.get_term_topics(lemma_id, 1e-10)))
 
     # Save terms
     with open(output, 'wb') as fp:
@@ -44,27 +46,47 @@ def get_terms_topics(model, idx_to_lemma, output):
             for topic_id, topic_prob in topic_distr:
                 fp.write('\t{}\t{}\n'.format(topic_id, topic_prob))
 
+    return terms_topics
+
+
+def get_document_topic_distribution(num_topics, data, corpus, model, output):
+    documents_topic_distr = []
+    for i, (item, text) in enumerate(data):
+        # item = fix_item_path(item, section) # To filter root path
+
+        # Get % of each topic per document
+        topic_distr = model.get_document_topics(corpus[i], 1e-10)
+        assert len(topic_distr) == num_topics
+        documents_topic_distr.append((item, topic_distr))
+
+    with open(output, 'wb') as fp:
+        pickle.dump(documents_topic_distr, fp)
+    with open(output + '.txt', 'w', encoding='utf-8') as fp:
+        for item, distr in documents_topic_distr:
+            fp.write(item + '\t' + ' '.join([str(x) for x in distr]) + '\n')
+
+    return documents_topic_distr
+
 
 if __name__ == "__main__":
     #logging.getLogger().setLevel(logging.INFO)
     numpy.random.seed(config.SEED)
     random.seed(config.SEED)
 
-    sections_to_analyze = [config.DATA_1A_FOLDER]
+    if not os.path.exists(config.DATA_DISTRIBUTION_FOLDER):
+        os.makedirs(config.DATA_DISTRIBUTION_FOLDER)
+
+    sections_to_analyze = [config.DATA_1A_FOLDER, config.DATA_7_FOLDER, config.DATA_7A_FOLDER]
     for section in sections_to_analyze:
         data = analyze_topics_static.load_and_clean_data(section)
         data, lemma_to_idx, idx_to_lemma = analyze_topics_static.preprocess(section, data)
-        corpus, dictionary, texts, time_slices = analyze_topics_static.preprocessing_topic(data, idx_to_lemma)
+        corpus, dictionary, texts, time_slices, data = analyze_topics_static.preprocessing_topic(data, idx_to_lemma)
 
         model_filepath = config.TRAIN_PARAMETERS[section][1]
         assert os.path.exists(model_filepath)
         model, c_v, u_mass = analyze_topics_static.train_topic_model_or_load(corpus, dictionary, texts, model_file=model_filepath, only_viz=True)
 
-        get_topics(config.TRAIN_PARAMETERS[section][0], model, dictionary.id2token, config.TRAIN_PARAMETERS[section][5])
-        get_terms_topics(model, dictionary.id2token, config.TRAIN_PARAMETERS[section][5].replace(config.TOPIC_EXTENSION, config.TERMS_EXTENSION))
-
-        for item, text in data:
-            item = fix_item_path(item, section)
-
-            # Get % of each topic per document
-            # TODO model.get_document_topics
+        num_topics = config.TRAIN_PARAMETERS[section][0]
+        topics = get_topics(num_topics, model, dictionary.id2token, config.TRAIN_PARAMETERS[section][5])
+        terms_topics = get_terms_topics(model, dictionary.id2token, config.TRAIN_PARAMETERS[section][5].replace(config.TOPIC_EXTENSION, config.TERMS_EXTENSION))
+        documents_topic_distr = get_document_topic_distribution(num_topics, data, corpus, model, config.TRAIN_PARAMETERS[section][5].replace(config.TOPIC_EXTENSION, config.DOCS_EXTENSION))
