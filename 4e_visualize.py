33765b8c5d9baa0510020b2ce0ad45e51967d03e
Diego999 <diegoantognini@gmail.com>
2017-09-04 10:32:54 +0200

    Update parameters

--- 4e_visualize.py
+++ 4e_visualize.py
@@ -131,7 +131,7 @@ def train_or_load_mds(mds_filepath, vals, seed=config.SEED):
             mds_lda = pickle.load(fp)
         # Too big to load
     else:
-        mds_model = MDS(n_components=2, max_iter=30, verbose=3, n_init=4, n_jobs=4, dissimilarity='euclidean', random_state=seed)
+        mds_model = MDS(n_components=2, max_iter=20, verbose=3, n_init=4, n_jobs=4, dissimilarity='euclidean', random_state=seed)
         mds_lda = mds_model.fit_transform(vals)
         #Too big to save the model
         with open(mds_filepath, 'wb') as fp:
