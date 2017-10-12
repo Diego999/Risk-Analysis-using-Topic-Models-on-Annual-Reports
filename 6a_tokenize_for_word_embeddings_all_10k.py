6110876dc67586ae8b651873dfe238fa2f447d3e
Diego999 <diegoantognini@gmail.com>
2017-10-12 08:42:09 +0200

    Add word embedding preprocessing with all annual reports

--- 6a_tokenize_for_word_embeddings_all_10k.py
+++ 6a_tokenize_for_word_embeddings_all_10k.py
@@ -5,10 +5,12 @@ import random
 import re
 import utils
 import stanford_corenlp_pywrapper
+import glob
 from multiprocessing import Process, Manager
 analyze_topics_static = __import__('4a_analyze_topics_static')
 infer_topics = __import__('4d_infer_topics')
 config = __import__('0_config')
+
 
 SENTENCE_DELIMITER = ' <DELIMITER> '
 pattern_remove_multiple_space = re.compile(r'\s+')
@@ -22,6 +24,7 @@ pattern_remove_multiple_dash = re.compile(r'-+')
 pattern_map_digit_to_hash = re.compile(r'[0-9]+')
 pattern_html_tags = re.compile('<.*?>')
 pattern_multiple_hash = re.compile(r'#+')
+pattern_multiple_equal_underline = re.compile(r'[=_]+')
 pattern_dot_among_hashes = re.compile(r'#+ *. *#+')
 pattern_comma_among_hashes = re.compile(r'#+ *, *#+')
 remove_numbers_redundancy = re.compile(r'[% -]*#+[% -]*')
@@ -39,6 +42,7 @@ def clean(buffer, KEYWORDS_TO_DETECT, MIN_LENGTH_EMPTY, MIN_LENGTH_KEYWORDS, MIN
     text = re.sub(pattern_remove_quote_2, '', text)
     text = re.sub(pattern_remove_quote_3, '', text)
     text = re.sub(pattern_remove_quote_4, '', text)
+    text = re.sub(pattern_multiple_equal_underline, '', text)
     text = re.sub(pattern_non_char_digit_hash_perc_dash, ' ', text)
     text = re.sub(pattern_map_digit_to_hash, '#', text)
     text = re.sub(pattern_remove_multiple_dash, '-', text)
@@ -74,20 +78,6 @@ def clean(buffer, KEYWORDS_TO_DETECT, MIN_LENGTH_EMPTY, MIN_LENGTH_KEYWORDS, MIN
     text = re.sub(pattern_remove_multiple_space, ' ', text)
     text = text.strip()
 
-    start_over = True
-    while start_over:
-        start_over = False
-        for remove_start_words in REMOVE_START_WORDS:
-            cond_valid = text.startswith(remove_start_words)
-            if not cond_valid:
-                remove_start_words = remove_start_words.replace('#', 'NUM ').replace('.', '')
-                cond_valid = text.startswith(remove_start_words)
-            if cond_valid:
-                text = text[len(remove_start_words) + 1:]
-                while text[0] in [' ', '-', '#']:
-                    text = text[1:]
-                start_over = True
-
     return text.strip()
 
 
@@ -116,7 +106,7 @@ def nlp_process(text, annotator):
     return SENTENCE_DELIMITER.join(tokens_sent)
 
 
-def load_and_clean_data_process(items, section, storage, pid):
+def load_and_clean_data_process(items, output_folder, storage, pid):
     cleaned_data = []
     annotator = stanford_corenlp_pywrapper.CoreNLP(configdict={'annotators': 'tokenize, ssplit'}, corenlp_jars=[config.SF_NLP_JARS])
     for i, item in enumerate(items):
@@ -125,36 +115,34 @@ def load_and_clean_data_process(items, section, storage, pid):
             for l in fp:
                 buffer.append(l)
 
-        buffer = analyze_topics_static.remove_header_and_multiple_lines(buffer)
-        if analyze_topics_static.content_relevant(buffer, *config.CLEAN_PARAMETERS[section]):
-            cleaned_data.append((item, nlp_process(clean(buffer, *config.CLEAN_PARAMETERS[section]), annotator)))
+        if len(buffer) > (200 + 250):
+            buffer = buffer[200:-250]
+            buffer = analyze_topics_static.remove_header_and_multiple_lines(buffer)
+            cleaned_data.append((item, nlp_process(clean(buffer, None, None, None, None, None), annotator)))
 
     storage[pid] = cleaned_data
 
 
-def load_and_clean_data(section):
-    cleaned_data_file = section + config.SUFFIX_CLEAN_DATA + config.SUFFIX_PREPROCESSED_DATA_FOR_WE.replace('pkl', 'txt')
+def load_and_clean_data(output_folder, reports):
+    cleaned_data_file = output_folder + '/all_10k_' + config.SUFFIX_CLEAN_DATA
     cleaned_data = []
     if config.FORCE_PREPROCESSING or not os.path.exists(cleaned_data_file):
-        items = analyze_topics_static.read_section(section)
         manager = Manager()
         if config.MULTITHREADING:
-            items_chunk = utils.chunks(items, 1 + int(len(items) / config.NUM_CORES))
+            items_chunk = utils.chunks(reports, 1 + int(len(reports) / config.NUM_CORES))
             final_data = manager.list([[]] * config.NUM_CORES)
 
             procs = []
             for i in range(config.NUM_CORES):
-                procs.append(Process(target=load_and_clean_data_process, args=(items_chunk[i], section, final_data, i)))
+                procs.append(Process(target=load_and_clean_data_process, args=(items_chunk[i], output_folder, final_data, i)))
                 procs[-1].start()
 
             for p in procs:
                 p.join()
         else:
             final_data = [None]
-            load_and_clean_data_process(items, section, final_data, 0)
+            load_and_clean_data_process(reports, output_folder, final_data, 0)
         cleaned_data = sum(final_data, [])
-
-        print('{}: Keep {} over {} ({:.2f}%)'.format(section[section.rfind('/') + 1:], len(cleaned_data), len(items), float(len(cleaned_data)) / len(items) * 100.0))
 
         with open(cleaned_data_file, 'w', encoding='utf-8') as fp:
             for i, l in cleaned_data:
@@ -173,15 +161,12 @@ if __name__ == "__main__":
     numpy.random.seed(config.SEED)
     random.seed(config.SEED)
 
-    sections_to_analyze = [config.DATA_1A_FOLDER]
-    for section in sections_to_analyze:
-        path = os.path.join(config.OUTPUT_FOLDER_WORD_EMB, section[section.rfind('/')+1:])
-        if not os.path.exists(path):
-            os.makedirs(path)
+    reports = glob.glob(config.DATA_AR_FOLDER + '/*/*.' + config.EXTENSION_CLEAN_PREPROCESSING)
 
-        data = load_and_clean_data(section)
-        output = section + config.SUFFIX_CLEAN_DATA + config.SUFFIX_PREPROCESSED_DATA_FOR_WE.replace('pkl', 'txt')
-        with open(output, 'w', encoding='utf-8') as fp:
-            for item, text in data:
-                for sent in text.split(SENTENCE_DELIMITER):
-                    fp.write(sent + '\n')
+    data = load_and_clean_data(config.DATA_FOLDER, reports)
+    output = config.DATA_FOLDER + '/all_10k_' + config.SUFFIX_CLEAN_DATA + config.SUFFIX_PREPROCESSED_DATA_FOR_WE.replace('pkl', 'txt')
+    with open(output, 'w', encoding='utf-8') as fp:
+        for item, text in data:
+            for sent in text.split(SENTENCE_DELIMITER):
+                fp.write(sent + '\n')
+
