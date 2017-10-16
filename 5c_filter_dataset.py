0dc5d564a5473d512981d3f41ba7cc39fa20da2a
Diego999 <diegoantognini@gmail.com>
2017-10-16 20:38:42 +0200

    Add dataset filtering

---
+++ 5c_filter_dataset.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import numpy as np
+config = __import__('0_config')
+
+
+if __name__ == "__main__":
+    sections_to_analyze = [config.DATA_1A_FOLDER, config.DATA_7A_FOLDER, config.DATA_7_FOLDER]
+    key = 'volatility'
+    for section in sections_to_analyze:
+        data = pd.read_pickle(os.path.join(section, 'data_df.pkl'))
+        data.replace([np.inf, -np.inf], np.nan)
+        data = data[pd.isnull(data[key]) == False]
+        pd.to_pickle(os.path.join(section, 'data_df_filtered.pkl'))
+
