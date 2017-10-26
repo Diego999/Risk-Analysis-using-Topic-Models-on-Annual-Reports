329b65d77609df9bb80ddb541806c416a39b4af8
Diego999 <diegoantognini@gmail.com>
2017-10-26 10:40:48 +0200

    Refactor

--- 4c_analyze_topics_through_time.py
+++ 4c_analyze_topics_through_time.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         assert os.path.exists(model_file)
         num_topics = config.TRAIN_PARAMETERS[section][0]
         model, c_v, u_mass = analyze_topics_static.train_topic_model_or_load(corpus, dictionary, texts, num_topics=num_topics, alpha='symmetric', eta='auto', chunksize=2000, decay=0.5, offset=1.0, passes=10, iterations=400, eval_every=10, model_file=model_file, only_viz=config.DO_NOT_COMPUTE_COHERENCE)
-        print('c_v:' + str(round(c_v, 4)) + ', cu:' + str(round(u_mass, 4)))
+        print('topic:' + str(num_topics), ', time_slice' + ' '.join([str(i) + ':' + str(j) for i,v in enumerate(time_slices)]) + ', c_v:' + str(round(c_v, 4)) + ', cu:' + str(round(u_mass, 4)))
 
         dyn_model = ldaseqmodel.LdaSeqModel(initialize='ldamodel', lda_model=model, time_slice=time_slices, corpus=corpus, id2word=dictionary, num_topics=num_topics, passes=10, random_state=config.SEED)
         filename = config.TRAIN_PARAMETERS[section][3]
@@ -47,5 +47,5 @@ if __name__ == "__main__":
 
             loc_dot_ext = filename.rfind('.')
             year = config.START_YEAR + t
-            filename = filename[:loc_dot_ext] + '_{}'.format(year) + filename[filename.rfind('.'):]
-            pyLDAvis.save_html(prepared, filename + '.html')
+            filename_dyn = filename[:loc_dot_ext] + '_{}'.format(year) + filename[filename.rfind('.'):]
+            pyLDAvis.save_html(prepared, filename_dyn + '.html')
